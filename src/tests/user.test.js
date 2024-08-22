5b67d0db4c89ce4f2afa7e728fd5bc8191b5294c
César Guerrero <grocksta@gmail.com>
2024-08-21 18:38:29 -0600

    capturetoken

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,6 +1,21 @@
-const User = require("../models/User");
+const request  = require("supertest")
+const app = require('../app')
 
 const BASE_URL = '/api/v1/users'
+
+//obteniendo hits
+beforeAll(async () => {
+    const user = {
+        email: "angus@gmail.com",
+        password: "angus1234"
+    }
+
+    const res = await request(app)
+       .post(`${BASE_URL}/`)
+       .send(user)
+
+       TOKEN = res.body.TOKEN
+})
 
 const user = {
     firstName: "Pepe",
@@ -12,6 +27,16 @@ const user = {
 
 test("POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName", async () => {
 
-    const user = await User.findAll()
-    console.log(user);
+   const columns = ['firstName', 'lastName', 'email', 'password', 'phone']  //resolver esto con foreach
+   const res = await request(app)
+     .post(BASE_URL)
+     .send(user)
+//se puede aplicar un for each 
+     expect(res.statusCode).toBe(201)
+     expect(res.body).toBeDefined()
+    //  columns.foreach((column) => {        
+        
+    //  }) 
+     expect(res.body.firstName).toBeDefined()
+     expect(res.body.firstName).toBe(user.firstName)
 })
