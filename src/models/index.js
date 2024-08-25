ee78b9bc235a02d1f100c7832e4493e888aed6bc
César Guerrero <grocksta@gmail.com>
2024-08-24 22:22:47 -0600

    relaciones

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,7 @@
 const User = require("./User") //importamos el modelo de user
 const Category = require("./Category")//importamos Category
 const Product = require("./Product");
+
+// product --> categoryId relacion
+Product.belongsTo(Category)
+Category.hasMany(Product)
